b702a47c621fe77e3c192476bf67757db75c4069
Kyleferg6 <krf6081@g.rit.edu>
2019-10-05 17:48:58 -0400

    Drone connect working? <3 Kyle

--- Networking/Networking.py
+++ Networking/Networking.py
@@ -9,6 +9,7 @@ The networking framework for our drones.  Middleman for the drones and the field
 """
 
 import socket  # for sockets
+from time import sleep
 
 # # # # # # # # # # # # # # # # # # Networking Thoughts Box # # # # # # # # # # # # # # # # # # # # #
 # Drones need to:                                                                                   #
@@ -16,22 +17,35 @@ import socket  # for sockets
 #       -then send new position new velocity, request to the field for locations of close drones    #
 #       -accept array list of drone locations from field                                            #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-fieldIP = '192.168.137.13' #1036.252.145 209.217.218.34
+
+
+fieldIP = '192.168.137.5' #1036.252.145 209.217.218.34
 fieldPort = 6666
 
 
 def drone_connect():
+    """
+
+    :return:
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((fieldIP, fieldPort))
     print("connected")
     return s
 
 
+def drone_send_info():
+    print()
+
+
 def main():
     s = drone_connect()
-    s.sendall(str.encode("*Notices your bulge* OwO"))
-    data = s.recv(1024)  # same buffsize
-    print(str(data))
+    for i in range(10):
+        s.sendall(str.encode(str(i)))
+        sleep(1)
+    # data = s.recv(1024)  # same buffsize
+    # print(str(data))
+    s.sendall(str.encode("*FULLSTOP*"))
 
 
 if __name__ == '__main__':
