d8939014a80b39c8dad2ee64d44ef82b1897b6d1
Takatsuka-mark <takatsuka.mark@gmail.com>
2019-10-05 17:27:32 -0400

    Fixing something for ionis

--- Visualization/Space.py
+++ Visualization/Space.py
@@ -21,15 +21,15 @@ def client(client_socket, addr):
 def init():
     global SOCK
     SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    SOCK.bind(('192.168.137.13', 6666))
+    SOCK.bind(('192.168.137.5', 6666))
     SOCK.listen(5)
 
 
 def main():
+    client_sock, addr = SOCK.accept()
     for i in range(0, 5):
-        client_sock, addr = SOCK.accept()
         print("Connected at: ", addr)
-        print(SOCK.recvmsg())
+        print(client_sock.recv(1024))
     time.sleep(1)
 
 
