1e2b3becfe8841bd97b7afb2598aedf8ae9411d2
Kyleferg6 <krf6081@g.rit.edu>
2019-10-05 20:46:07 -0400

    Fixed circular import error <3 Kyle

--- Networking/Networking.py
+++ Networking/Networking.py
@@ -8,7 +8,6 @@ Python Version: 3.7
 The networking framework for our drones.  Middleman for the drones and the field.
 """
 
-# from Drones.Drone import DroneData # for testing REMOVE LATER
 import socket  # for sockets
 import pickle  # for sending drone data to server
 from time import sleep
@@ -19,7 +18,6 @@ from time import sleep
 #       -then send new position new velocity, request to the field for locations of close drones    #
 #       -accept array list of drone locations from field                                            #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
 
 fieldIP = '192.168.137.5' #1036.252.145 209.217.218.34
 fieldPort = 6666
@@ -50,28 +48,3 @@ def drone_send_info(s, data):
     """
     data_string = pickle.dumps(data)
     return s.send(data_string)
-
-
-# def main():
-#     dronelist = []
-#     for i in range(1):
-#         dronelist.append(drone_connect())
-# 
-#     for i in range(10):
-#         data = DroneData()
-#         data.id = ""
-#         num = 0.0
-#         data.position = (num, num, num)
-#         data.velocity = (num, num, num)
-#         print(drone_send_info(dronelist[0], data))
-#         # sleep(1)
-#     dronelist[0].close
-
-    # print(drone_send_info(s2, "drone 2 checking in"))
-    # s2.close()
-    # print(drone_send_info(s3, "drone 3 checking in"))
-    # s3.close()
-
-
-# if __name__ == '__main__':
-#     main()
